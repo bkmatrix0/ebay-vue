2f6f9b1c6f81fcf950f0d63cf2d850a56b3e7162
bkmatrix0 <bkmatrix99@naver.com>
2019-05-09 20:34:33 +0900

    commit [ebay-pre-test]-myjung

---
+++ pre-course-test/정명환/components/HelloVue.vue
@@ -0,0 +1,18 @@
+<template>
+    <p>
+        hello vue {{val}
+        <button v-on:click="sendEvent()">5로 변경</button>
+    </p>
+</template>
+
+<script>
+export default {
+    name: 'HelloVue',
+    props: ['val'],
+    methods: {
+        sendEvent() {
+            this.$emit("change", 5);
+        }
+    }
+}
+</script>
